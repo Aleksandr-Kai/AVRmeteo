2d184446c59a36e72d0399804f06a17fde34f6df
Aleksandr.Kai <timofeev.aleksandr.mail@gmail.com>
2022-12-06 16:00:54 +0300

    fix uart reciver

--- meteo_conf.c
+++ meteo_conf.c
@@ -9,8 +9,8 @@
 
 // 1 Wire Bus functions
 #asm
-   .equ __w1_port=0x12 ;PORTD
-   .equ __w1_bit=3
+.equ __w1_port = 0x12
+					 .equ __w1_bit = 3
 #endasm
 
 // DS1820 Temperature Sensor functions
@@ -19,9 +19,9 @@
 // maximum number of DS1820 devices
 // connected to the 1 Wire bus
 #define MAX_DS1820 8
-// number of DS1820 devices
-// connected to the 1 Wire bus
-unsigned char ds1820_devices;
+	// number of DS1820 devices
+	// connected to the 1 Wire bus
+	unsigned char ds1820_devices;
 // DS1820 devices ROM code storage area,
 // 9 bytes are used for each device
 // (see the w1_search function description in the help)
@@ -30,50 +30,49 @@ unsigned char ds1820_rom_codes[MAX_DS1820][9];
 int dots_cnt = 0;
 char blinkDots = 1;
 // Timer 0 overflow interrupt service routine
-interrupt [TIM0_OVF] void timer0_ovf_isr(void)
+interrupt[TIM0_OVF] void timer0_ovf_isr(void)
 {
 	// Reinitialize Timer 0 value
-	//TCNT0=0x38;
-	TCNT0=0x06;
+	// TCNT0=0x38;
+	TCNT0 = 0x06;
 	Dyn_Indicate();
-	if (++dots_cnt == 500) {
-		if(PORTA.3 && blinkDots) PORTA.3 = 0; else PORTA.3 = 1;
+	if (++dots_cnt == 500)
+	{
+		if (PORTA .3 && blinkDots)
+			PORTA .3 = 0;
+		else
+			PORTA .3 = 1;
 		dots_cnt = 0;
 	}
 }
 
-//*********************************************************************************************************************
 
-int outtemp;
-char outtempready = 0;
-
-//*********************************************************************************************************************
-
-void chipinit() {
+void chipinit()
+{
 	// Input/Output Ports initialization
 	// Port A initialization
-	// Function: Bit7=In Bit6=In Bit5=In Bit4=In Bit3=Out Bit2=Out Bit1=Out Bit0=Out 
-	DDRA=(0<<DDA7) | (0<<DDA6) | (0<<DDA5) | (0<<DDA4) | (1<<DDA3) | (1<<DDA2) | (1<<DDA1) | (1<<DDA0);
-	// State: Bit7=T Bit6=T Bit5=T Bit4=T Bit3=0 Bit2=0 Bit1=0 Bit0=0 
-	PORTA=(1<<PORTA7) | (0<<PORTA6) | (0<<PORTA5) | (0<<PORTA4) | (0<<PORTA3) | (0<<PORTA2) | (0<<PORTA1) | (0<<PORTA0);
+	// Function: Bit7=In Bit6=In Bit5=In Bit4=In Bit3=Out Bit2=Out Bit1=Out Bit0=Out
+	DDRA = (0 << DDA7) | (0 << DDA6) | (0 << DDA5) | (0 << DDA4) | (1 << DDA3) | (1 << DDA2) | (1 << DDA1) | (1 << DDA0);
+	// State: Bit7=T Bit6=T Bit5=T Bit4=T Bit3=0 Bit2=0 Bit1=0 Bit0=0
+	PORTA = (1 << PORTA7) | (0 << PORTA6) | (0 << PORTA5) | (0 << PORTA4) | (0 << PORTA3) | (0 << PORTA2) | (0 << PORTA1) | (0 << PORTA0);
 
 	// Port B initialization
-	// Function: Bit7=Out Bit6=Out Bit5=Out Bit4=Out Bit3=Out Bit2=Out Bit1=Out Bit0=Out 
-	DDRB=(1<<DDB7) | (1<<DDB6) | (1<<DDB5) | (1<<DDB4) | (1<<DDB3) | (1<<DDB2) | (1<<DDB1) | (1<<DDB0);
-	// State: Bit7=0 Bit6=0 Bit5=0 Bit4=0 Bit3=0 Bit2=0 Bit1=0 Bit0=0 
-	PORTB=(0<<PORTB7) | (0<<PORTB6) | (0<<PORTB5) | (0<<PORTB4) | (0<<PORTB3) | (0<<PORTB2) | (0<<PORTB1) | (0<<PORTB0);
+	// Function: Bit7=Out Bit6=Out Bit5=Out Bit4=Out Bit3=Out Bit2=Out Bit1=Out Bit0=Out
+	DDRB = (1 << DDB7) | (1 << DDB6) | (1 << DDB5) | (1 << DDB4) | (1 << DDB3) | (1 << DDB2) | (1 << DDB1) | (1 << DDB0);
+	// State: Bit7=0 Bit6=0 Bit5=0 Bit4=0 Bit3=0 Bit2=0 Bit1=0 Bit0=0
+	PORTB = (0 << PORTB7) | (0 << PORTB6) | (0 << PORTB5) | (0 << PORTB4) | (0 << PORTB3) | (0 << PORTB2) | (0 << PORTB1) | (0 << PORTB0);
 
 	// Port C initialization
-	// Function: Bit7=Out Bit6=Out Bit5=Out Bit4=Out Bit3=Out Bit2=Out Bit1=Out Bit0=Out 
-	DDRC=(1<<DDC7) | (1<<DDC6) | (1<<DDC5) | (1<<DDC4) | (1<<DDC3) | (1<<DDC2) | (1<<DDC1) | (1<<DDC0);
-	// State: Bit7=0 Bit6=0 Bit5=0 Bit4=0 Bit3=0 Bit2=0 Bit1=0 Bit0=0 
-	PORTC=(0<<PORTC7) | (0<<PORTC6) | (0<<PORTC5) | (0<<PORTC4) | (0<<PORTC3) | (0<<PORTC2) | (0<<PORTC1) | (0<<PORTC0);
+	// Function: Bit7=Out Bit6=Out Bit5=Out Bit4=Out Bit3=Out Bit2=Out Bit1=Out Bit0=Out
+	DDRC = (1 << DDC7) | (1 << DDC6) | (1 << DDC5) | (1 << DDC4) | (1 << DDC3) | (1 << DDC2) | (1 << DDC1) | (1 << DDC0);
+	// State: Bit7=0 Bit6=0 Bit5=0 Bit4=0 Bit3=0 Bit2=0 Bit1=0 Bit0=0
+	PORTC = (0 << PORTC7) | (0 << PORTC6) | (0 << PORTC5) | (0 << PORTC4) | (0 << PORTC3) | (0 << PORTC2) | (0 << PORTC1) | (0 << PORTC0);
 
 	// Port D initialization
-	// Function: Bit7=In Bit6=In Bit5=In Bit4=In Bit3=In Bit2=In Bit1=In Bit0=In 
-	DDRD=(1<<DDD7) | (0<<DDD6) | (0<<DDD5) | (0<<DDD4) | (0<<DDD3) | (0<<DDD2) | (0<<DDD1) | (0<<DDD0);
-	// State: Bit7=T Bit6=T Bit5=T Bit4=T Bit3=T Bit2=T Bit1=T Bit0=T 
-	PORTD=(0<<PORTD7) | (0<<PORTD6) | (0<<PORTD5) | (0<<PORTD4) | (0<<PORTD3) | (0<<PORTD2) | (0<<PORTD1) | (0<<PORTD0);
+	// Function: Bit7=In Bit6=In Bit5=In Bit4=In Bit3=In Bit2=In Bit1=In Bit0=In
+	DDRD = (1 << DDD7) | (0 << DDD6) | (0 << DDD5) | (0 << DDD4) | (0 << DDD3) | (0 << DDD2) | (0 << DDD1) | (0 << DDD0);
+	// State: Bit7=T Bit6=T Bit5=T Bit4=T Bit3=T Bit2=T Bit1=T Bit0=T
+	PORTD = (0 << PORTD7) | (0 << PORTD6) | (0 << PORTD5) | (0 << PORTD4) | (0 << PORTD3) | (0 << PORTD2) | (0 << PORTD1) | (0 << PORTD0);
 
 	// Timer/Counter 0 initialization
 	// Clock source: System Clock
@@ -81,9 +80,9 @@ void chipinit() {
 	// Mode: Normal top=0xFF
 	// OC0 output: Disconnected
 	// Timer Period: 1 us
-	TCCR0=(0<<WGM00) | (0<<COM01) | (0<<COM00) | (0<<WGM01) | (0<<CS02) | (1<<CS01) | (1<<CS00);
-	TCNT0=0xF0;
-	OCR0=0x00;
+	TCCR0 = (0 << WGM00) | (0 << COM01) | (0 << COM00) | (0 << WGM01) | (0 << CS02) | (1 << CS01) | (1 << CS00);
+	TCNT0 = 0xF0;
+	OCR0 = 0x00;
 
 	// Timer/Counter 1 initialization
 	// Clock source: System Clock
@@ -97,16 +96,16 @@ void chipinit() {
 	// Input Capture Interrupt: Off
 	// Compare A Match Interrupt: Off
 	// Compare B Match Interrupt: Off
-	TCCR1A=(0<<COM1A1) | (0<<COM1A0) | (0<<COM1B1) | (0<<COM1B0) | (0<<WGM11) | (0<<WGM10);
-	TCCR1B=(0<<ICNC1) | (0<<ICES1) | (0<<WGM13) | (0<<WGM12) | (0<<CS12) | (0<<CS11) | (0<<CS10);
-	TCNT1H=0x00;
-	TCNT1L=0x00;
-	ICR1H=0x00;
-	ICR1L=0x00;
-	OCR1AH=0x00;
-	OCR1AL=0x00;
-	OCR1BH=0x00;
-	OCR1BL=0x00;
+	TCCR1A = (0 << COM1A1) | (0 << COM1A0) | (0 << COM1B1) | (0 << COM1B0) | (0 << WGM11) | (0 << WGM10);
+	TCCR1B = (0 << ICNC1) | (0 << ICES1) | (0 << WGM13) | (0 << WGM12) | (0 << CS12) | (0 << CS11) | (0 << CS10);
+	TCNT1H = 0x00;
+	TCNT1L = 0x00;
+	ICR1H = 0x00;
+	ICR1L = 0x00;
+	OCR1AH = 0x00;
+	OCR1AL = 0x00;
+	OCR1BH = 0x00;
+	OCR1BL = 0x00;
 
 	// Timer/Counter 2 initialization
 	// Clock source: System Clock
@@ -114,29 +113,29 @@ void chipinit() {
 	// Mode: Normal top=0xFF
 	// OC2 output: Disconnected
 	// Timer Period: 9.984 ms
-	ASSR=0<<AS2;
-	TCCR2=(0<<PWM2) | (0<<COM21) | (0<<COM20) | (0<<CTC2) | (1<<CS22) | (1<<CS21) | (1<<CS20);
-	TCNT2=0x64;
-	OCR2=0x00;
+	ASSR = 0 << AS2;
+	TCCR2 = (0 << PWM2) | (0 << COM21) | (0 << COM20) | (0 << CTC2) | (1 << CS22) | (1 << CS21) | (1 << CS20);
+	TCNT2 = 0x64;
+	OCR2 = 0x00;
 
 	// Timer(s)/Counter(s) Interrupt(s) initialization
-	TIMSK=(0<<OCIE2) | (1<<TOIE2) | (0<<TICIE1) | (0<<OCIE1A) | (0<<OCIE1B) | (0<<TOIE1) | (0<<OCIE0) | (1<<TOIE0);
+	TIMSK = (0 << OCIE2) | (1 << TOIE2) | (0 << TICIE1) | (0 << OCIE1A) | (0 << OCIE1B) | (0 << TOIE1) | (0 << OCIE0) | (1 << TOIE0);
 
 	// External Interrupt(s) initialization
 	// INT0: Off
 	// INT1: Off
 	// INT2: Off
-	MCUCR=(0<<ISC11) | (0<<ISC10) | (0<<ISC01) | (0<<ISC00);
-	MCUCSR=(0<<ISC2);
+	MCUCR = (0 << ISC11) | (0 << ISC10) | (0 << ISC01) | (0 << ISC00);
+	MCUCSR = (0 << ISC2);
 
 	// SPI initialization
 	// SPI disabled
-	SPCR=(0<<SPIE) | (0<<SPE) | (0<<DORD) | (0<<MSTR) | (0<<CPOL) | (0<<CPHA) | (0<<SPR1) | (0<<SPR0);
+	SPCR = (0 << SPIE) | (0 << SPE) | (0 << DORD) | (0 << MSTR) | (0 << CPOL) | (0 << CPHA) | (0 << SPR1) | (0 << SPR0);
 
 	// TWI initialization
 	// TWI disabled
-	TWCR=(0<<TWEA) | (0<<TWSTA) | (0<<TWSTO) | (0<<TWEN) | (0<<TWIE);
-	
+	TWCR = (0 << TWEA) | (0 << TWSTA) | (0 << TWSTO) | (0 << TWEN) | (0 << TWIE);
+
 	InitADC();
 	InitUART();
 
@@ -149,10 +148,10 @@ void chipinit() {
 
 	// Determine the number of DS1820 devices
 	// connected to the 1 Wire bus
-	ds1820_devices=w1_search(0xf0,ds1820_rom_codes);
+	ds1820_devices = w1_search(0xf0, ds1820_rom_codes);
 	// Global enable interrupts
 
 	i2c_init();
-	
-	#asm("sei")
+
+#asm("sei")
 }
--- uart.c
+++ uart.c
@@ -1,36 +1,42 @@
-// Declare your global variables here
 
-#define DATA_REGISTER_EMPTY (1<<UDRE)
-#define RX_COMPLETE (1<<RXC)
-#define FRAMING_ERROR (1<<FE)
-#define PARITY_ERROR (1<<UPE)
-#define DATA_OVERRUN (1<<DOR)
+//*********************************************************************************************************************
+char temprcv = 0;
+int outtemp;
+char outtempready = 0;
+//*********************************************************************************************************************
+
+#define DATA_REGISTER_EMPTY (1 << UDRE)
+#define RX_COMPLETE (1 << RXC)
+#define FRAMING_ERROR (1 << FE)
+#define PARITY_ERROR (1 << UPE)
+#define DATA_OVERRUN (1 << DOR)
 
 // USART Transmitter buffer
 #define TX_BUFFER_SIZE 8
 char tx_buffer[TX_BUFFER_SIZE];
 
 #if TX_BUFFER_SIZE <= 256
-unsigned char tx_wr_index=0,tx_rd_index=0;
+unsigned char tx_wr_index = 0, tx_rd_index = 0;
 #else
-unsigned int tx_wr_index=0,tx_rd_index=0;
+unsigned int tx_wr_index = 0, tx_rd_index = 0;
 #endif
 
 #if TX_BUFFER_SIZE < 256
-unsigned char tx_counter=0;
+unsigned char tx_counter = 0;
 #else
-unsigned int tx_counter=0;
+unsigned int tx_counter = 0;
 #endif
 
 // USART Transmitter interrupt service routine
-interrupt [USART_TXC] void usart_tx_isr(void)
+interrupt[USART_TXC] void usart_tx_isr(void)
 {
-if (tx_counter)
+   if (tx_counter)
    {
-   --tx_counter;
-   UDR=tx_buffer[tx_rd_index++];
+      --tx_counter;
+      UDR = tx_buffer[tx_rd_index++];
 #if TX_BUFFER_SIZE != 256
-   if (tx_rd_index == TX_BUFFER_SIZE) tx_rd_index=0;
+      if (tx_rd_index == TX_BUFFER_SIZE)
+         tx_rd_index = 0;
 #endif
    }
 }
@@ -38,39 +44,135 @@ if (tx_counter)
 #ifndef _DEBUG_TERMINAL_IO_
 // Write a character to the USART Transmitter buffer
 #define _ALTERNATE_PUTCHAR_
-#pragma used+
+#pragma used +
 void putchar(char c)
 {
-while (tx_counter == TX_BUFFER_SIZE);
+   while (tx_counter == TX_BUFFER_SIZE)
+      ;
 #asm("cli")
-if (tx_counter || ((UCSRA & DATA_REGISTER_EMPTY)==0))
+   if (tx_counter || ((UCSRA & DATA_REGISTER_EMPTY) == 0))
    {
-   tx_buffer[tx_wr_index++]=c;
+      tx_buffer[tx_wr_index++] = c;
 #if TX_BUFFER_SIZE != 256
-   if (tx_wr_index == TX_BUFFER_SIZE) tx_wr_index=0;
+      if (tx_wr_index == TX_BUFFER_SIZE)
+         tx_wr_index = 0;
 #endif
-   ++tx_counter;
+      ++tx_counter;
    }
-else
-   UDR=c;
+   else
+      UDR = c;
 #asm("sei")
 }
-#pragma used-
+#pragma used -
+#endif
+
+//*************************************************************************************************
+// USART Receiver buffer
+#define RX_BUFFER_SIZE 8
+char rx_buffer[RX_BUFFER_SIZE];
+
+#if RX_BUFFER_SIZE <= 256
+unsigned char rx_wr_index = 0, rx_rd_index = 0;
+#else
+unsigned int rx_wr_index = 0, rx_rd_index = 0;
+#endif
+
+#if RX_BUFFER_SIZE < 256
+unsigned char rx_counter = 0;
+#else
+unsigned int rx_counter = 0;
+#endif
+
+// This flag is set on USART Receiver buffer overflow
+bit rx_buffer_overflow;
+
+// USART Receiver interrupt service routine
+interrupt[USART_RXC] void usart_rx_isr(void)
+{
+   char status, data;
+   status = UCSRA;
+   data = UDR;
+
+   switch (temprcv)
+   {
+   case 0:
+      if (data == 0x88)
+      {
+         temprcv = 1;
+         outtempready = 0;
+      }
+      break;
+   case 1:
+      outtemp = data;
+      outtemp = outtemp << 8;
+      temprcv = 2;
+      break;
+   case 2:
+      outtemp |= data;
+      temprcv = 0;
+      outtempready = 1;
+      break;
+
+   default:
+      break;
+   }
+
+   if ((status & (FRAMING_ERROR | PARITY_ERROR | DATA_OVERRUN)) == 0)
+   {
+
+      rx_buffer[rx_wr_index++] = data;
+#if RX_BUFFER_SIZE == 256
+      // special case for receiver buffer size=256
+      if (++rx_counter == 0)
+         rx_buffer_overflow = 1;
+#else
+      if (rx_wr_index == RX_BUFFER_SIZE)
+         rx_wr_index = 0;
+      if (++rx_counter == RX_BUFFER_SIZE)
+      {
+         rx_counter = 0;
+         rx_buffer_overflow = 1;
+      }
+#endif
+   }
+}
+
+#ifndef _DEBUG_TERMINAL_IO_
+// Get a character from the USART Receiver buffer
+#define _ALTERNATE_GETCHAR_
+#pragma used +
+char getchar(void)
+{
+   char data;
+   while (rx_counter == 0)
+      ;
+   data = rx_buffer[rx_rd_index++];
+#if RX_BUFFER_SIZE != 256
+   if (rx_rd_index == RX_BUFFER_SIZE)
+      rx_rd_index = 0;
+#endif
+#asm("cli")
+   --rx_counter;
+#asm("sei")
+   return data;
+}
+#pragma used -
 #endif
 
 // Standard Input/Output functions
 #include <stdio.h>
 
-void InitUART() {
-    // USART initialization
-    // Communication Parameters: 8 Data, 1 Stop, No Parity
-    // USART Receiver: Off
-    // USART Transmitter: On
-    // USART Mode: Asynchronous
-    // USART Baud Rate: 9600
-    UCSRA=(0<<RXC) | (0<<TXC) | (0<<UDRE) | (0<<FE) | (0<<DOR) | (0<<UPE) | (0<<U2X) | (0<<MPCM);
-    UCSRB=(0<<RXCIE) | (1<<TXCIE) | (0<<UDRIE) | (0<<RXEN) | (1<<TXEN) | (0<<UCSZ2) | (0<<RXB8) | (0<<TXB8);
-    UCSRC=(1<<URSEL) | (0<<UMSEL) | (0<<UPM1) | (0<<UPM0) | (0<<USBS) | (1<<UCSZ1) | (1<<UCSZ0) | (0<<UCPOL);
-    UBRRH=0x00;
-    UBRRL=0x67;
+void InitUART()
+{
+   // USART initialization
+   // Communication Parameters: 8 Data, 1 Stop, No Parity
+   // USART Receiver: On
+   // USART Transmitter: On
+   // USART Mode: Asynchronous
+   // USART Baud Rate: 9600
+   UCSRA = (0 << RXC) | (0 << TXC) | (0 << UDRE) | (0 << FE) | (0 << DOR) | (0 << UPE) | (0 << U2X) | (0 << MPCM);
+   UCSRB = (1 << RXCIE) | (1 << TXCIE) | (0 << UDRIE) | (1 << RXEN) | (1 << TXEN) | (0 << UCSZ2) | (0 << RXB8) | (0 << TXB8);
+   UCSRC = (1 << URSEL) | (0 << UMSEL) | (0 << UPM1) | (0 << UPM0) | (0 << USBS) | (1 << UCSZ1) | (1 << UCSZ0) | (0 << UCPOL);
+   UBRRH = 0x00;
+   UBRRL = 0x67;
 }
